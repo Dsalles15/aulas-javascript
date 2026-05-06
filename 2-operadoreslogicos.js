2916313987bec9d21a623106678ccc3c53ffa77f
agnes dos reis <pedroafpovoa@gmail.com>
2026-05-06 19:33:31 -0300

    aula 11

---
+++ 2-operadoreslogicos.js
@@ -0,0 +1,33 @@
+//soma
+
+let numero1 = 10;
+let numero2 = 20;
+let soma = numero1 + numero2
+console.log(soma)
+
+//subtração
+let subtracao = numero1 - numero2
+console.log(subtracao)
+
+//multiplicaçao
+let multiplicacao = numero1 * numero2
+console.log(multiplicacao)
+
+//divisão
+let divisao = numero1 / numero2
+console.log(divisao)
+
+
+//resto da divisão 
+let restodiv = 10 % 3
+console.log(restodiv)
+
+//operadores aritimeticos
+//Condicionais
+
+if (numero % 2 == 0){
+  console.log("O "+numero1+ " é par")
+
+} else{
+     console.log("O"+numero1+"é impar")
+}
